d293040812bfc7c57ec04ed329d5a7775ef8b103
armando-pando-huaman <armandopando27@gmail.com>
2025-11-24 23:01:02 -0500

    actualización

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Armando Pando - Portfolio</h1>
+      <p>Prueba de conexión exitosa</p>
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppTest from './App-test.js';  // ← Usa el archivo de prueba
-import './styles/App.css';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppTest />
+    <App />
   </React.StrictMode>
 );
