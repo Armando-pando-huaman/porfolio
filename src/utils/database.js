b9577255b694952205cb59568b0efb863f163688
armando-pando-huaman <armandopando27@gmail.com>
2025-11-25 02:10:18 -0500

    cambio por cjs

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = "mongodb+srv://Armandopando:Nino.1412@cluster0.pmy61xe.mongodb.net/porfolio";
+const uri = "mongodb+srv://Armandopando:Nino.1412@cluster0.pmy6lxe.mongodb.net/porfolio";
 const options = {};
 
 let client;
