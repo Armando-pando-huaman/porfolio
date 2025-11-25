7f9897b8257fe26f19a731b534c73074e8102eac
armando-pando-huaman <armandopando27@gmail.com>
2025-11-25 03:40:26 -0500

    Update certifications.js

--- api/certifications.js
+++ api/certifications.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = "mongodb+srv://Armandopando:Nino.1412@cluster0.pmy6lxe.mongodb.net/porfolio?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://Armandopando:TestPassword123@cluster0.pmy6lxe.mongodb.net/porfolio?retryWrites=true&w=majority&appName=Cluster0";
 
 let cachedClient = null;
 
