f73ff804feabe05dfc1e8b624a57a74601d8bbca
armando-pando-huaman <armandopando27@gmail.com>
2025-11-26 18:31:38 -0500

    reparando

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        // Esto fuerza nombres de archivo consistentes
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
-    }
+    assetsDir: 'assets'
   },
-  // Esto es CRÍTICO para Vercel
-  base: './',
-  // Configuración explícita del server
-  server: {
-    host: true,
-    port: 3000
-  }
+  base: './'
 })
