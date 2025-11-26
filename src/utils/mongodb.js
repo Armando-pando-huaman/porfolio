b4c00e562d2fe362451500492be4571ab378a696
armando-pando-huaman <armandopando27@gmail.com>
2025-11-26 18:20:30 -0500

    comiit

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -1,35 +1,30 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb'
 
-const MONGODB_URI = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URL
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URL environment variable');
+  throw new Error('Please define the MONGODB_URL environment variable')
 }
 
-let cached = global.mongo;
+let cached = global.mongo
 
 if (!cached) {
-  cached = global.mongo = { conn: null, promise: null };
+  cached = global.mongo = { conn: null, promise: null }
 }
 
 export async function connectToDatabase() {
   if (cached.conn) {
-    return cached.conn;
+    return cached.conn
   }
 
   if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-
-    cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
+    cached.promise = MongoClient.connect(MONGODB_URI).then((client) => {
       return {
         client,
         db: client.db(),
-      };
-    });
+      }
+    })
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  cached.conn = await cached.promise
+  return cached.conn
 }
