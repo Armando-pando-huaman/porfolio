93e5cf2d15ee0b85d9ae9861e3d76e8efc133054
armando-pando-huaman <armandopando27@gmail.com>
2025-11-26 18:02:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
+    assetsDir: 'assets'
   },
-  base: '/',
+  base: './', // Esto hará que los recursos se carguen con rutas relativas
+  server: {
+    host: true
+  }
 })
