c74424340dc55f95e2bc8bb26a75ff005f92be06
armando-pando-huaman <armandopando27@gmail.com>
2025-11-26 14:18:26 -0500

    update vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
+  base: '/',
+  server: {
+    host: true
   }
 })
